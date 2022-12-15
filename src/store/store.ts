03da371d577a059f4e18a562ec82e526abc0ed34
Morozova Anastasiya <MorozovaAnastasiyaN@yandex.ru>
2022-12-15 14:37:04 +0400

    feat: add save state in localstorage

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,20 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
 import { counterV1Reducer } from "./reducers/counterV1Reducer";
 import { counterV2Reducer } from "./reducers/counterV2Reducer";
+import thunk from "redux-thunk";
+import { loadState, saveState } from "../helpers/localstorage";
 
 export const rootReducer = combineReducers({
   counterV1: counterV1Reducer,
   counterV2: counterV2Reducer,
 });
 
-export const store = createStore(rootReducer);
+export const store = createStore(
+  rootReducer,
+  loadState(),
+  applyMiddleware(thunk)
+);
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
