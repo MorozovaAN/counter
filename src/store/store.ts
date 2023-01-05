e87856b576f3b43cd7b365566468b34373983834
Morozova Anastasiya <MorozovaAnastasiyaN@yandex.ru>
2023-01-05 19:21:09 +0400

    refactor: switch to redux-toolkit, save values in localstorage

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,33 +1,18 @@
-// import { applyMiddleware, combineReducers, createStore } from "redux";
-// import { counterV1Reducer } from "./slices/counterV1Reducer";
-// import { counterV2Reducer } from "./slices/counterV2Reducer";
-// import thunk from "redux-thunk";
-// import { loadState, saveState } from "../helpers/localstorage";
-//
-// export const rootReducer = combineReducers({
-//   counterV1: counterV1Reducer,
-//   counterV2: counterV2Reducer,
-// });
-//
-// export const store = createStore(
-//   rootReducer,
-//   loadState(),
-//   applyMiddleware(thunk)
-// );
-//
-// store.subscribe(() => {
-//   saveState(store.getState());
-// });
-
 import { configureStore } from "@reduxjs/toolkit";
 import counterV1Reducer from "./slices/counterV1Slice";
 import counterV2Reducer from "./slices/counterV2Reducer";
+import { loadState, saveState } from "../helpers/localstorage";
 
 export const store = configureStore({
   reducer: {
     counterV1: counterV1Reducer,
     counterV2: counterV2Reducer,
   },
+  preloadedState: loadState(),
+});
+
+store.subscribe(() => {
+  saveState(store.getState());
 });
 
 export type RootStateType = ReturnType<typeof store.getState>;
